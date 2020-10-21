2cbd8b6273feafc6310052c55a8abeeb2fcf125e
VladSelikh <vladikh2405@gmail.com>
2020-10-21 17:24:09 +0300

    create new matrix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  
+  for (let i = 0; i < matrix.length; i++) {
+    if  (i % 2 === 1) {
+      matrix[i] = matrix[i].reverse();
+    }
+  }
+  let newMatrix = [];
+  for (let m = 0; m < matrix.length; m++) {
+    for (let n = 0; n < matrix.length; n++) {
+      newMatrix.push(matrix[m][n]);
+    }
+  }
   return newMatrix;
 }
